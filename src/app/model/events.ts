ddf10508e56035cc7b8f19b391976f315eb2887d
pavan <pavan89890@gmail.com>
2020-03-26 16:22:57 +0530

    app menu changes

--- src/app/model/events.ts
+++ src/app/model/events.ts
@@ -4,6 +4,7 @@ export class Events {
     public eventName:number;
     public eventDesc:string;
     public eventDateStr:string;
+    public today:boolean;
     public createdOn:Date;
     public updatedOn:Date;
 }
