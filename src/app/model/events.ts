4aaa73ad1a6823209832318dff07d2d4b0da2415
pavan <pavan89890@gmail.com>
2020-07-08 18:52:36 +0530

    removing event desc from the events screen

--- src/app/model/events.ts
+++ src/app/model/events.ts
@@ -2,7 +2,6 @@ export class Events {
     public id:string;
     public eventType:string;
     public eventName:number;
-    public eventDesc:string;
     public eventDateStr:string;
     public today:boolean;
     public createdOn:Date;
